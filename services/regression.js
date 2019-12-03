bdf6b5e67fb572066e89f85952d4bddefa9df6b1
sagarprabhu <sagarprabhu3@gmail.com>
2019-12-03 11:27:12 -0500

    added other attributes in regression

--- services/regression.js
+++ services/regression.js
@@ -20,16 +20,29 @@ router.post("/", function(request, res) {
     year: request.body.year
   };
   var attribute_to_name = {
-    n_killed: "People killed",
-    n_injured: "People injured",
-    n_guns_involved: "Total guns used"
+    UNEMPLOYMENT_RATE: "Unemployment rate",
+    LAWTOTAL: "Number of gun regulations",
+    HIGH_SCHOOL_GRADUATE: "Number people with High School",
+    BACHELORS_DEGREE: " Number of people with Bachelors degree"
   };
+  var attribute_to_table = {
+    LAWTOTAL: "FIREARMS_PROVISIONS",
+    HIGH_SCHOOL_GRADUATE: "DEMOGRAPHIC",
+    BACHELORS_DEGREE: "DEMOGRAPHIC"
+  };
+  var attribute_to_year_col = {
+    LAWTOTAL: "year",
+    HIGH_SCHOOL_GRADUATE: "DEMOGRAPHIC_YEAR",
+    BACHELORS_DEGREE: "DEMOGRAPHIC_YEAR"
+  };
+
   console.log(data);
 
   // console.log(data.date);
   var form_query = "";
   var binds = [];
-  form_query = `Select *
+  if (data.attribute == "UNEMPLOYMENT_RATE") {
+    form_query = `Select *
                 from
                 (select
                         x, y, us_state
@@ -91,7 +104,75 @@ router.post("/", function(request, res) {
                             ) datat
                         
                     ) r)`;
-
+  } else {
+    form_query = `Select *
+          from
+          (select
+                  x, y, t1.us_state
+                  from
+                    (select count(incident_id) as y, us_state from
+                              gun_incidents where incident_year = ${data.year} 
+                              group by us_state) t1,
+                              
+                                  (select ${data.attribute} as x, us_state from
+                              ${attribute_to_table[data.attribute]} where ${
+      attribute_to_year_col[data.attribute]
+    } = ${data.year}
+                              ) t2
+                              
+                          where t1.us_state = t2.us_state
+                ) 
+              union
+          (select
+            slope,
+            ybar - xbar * slope as intercept,
+              'coefficient' as coef
+          from
+            (
+              select
+                sum((x - xbar) * (y - ybar)) / sum((x - xbar) * (x - xbar)) as slope,
+                avg(ybar) as ybar,
+                avg(xbar) as xbar
+                    
+              from
+                (
+                  select
+                    avg(y) as ybar,
+                    avg(x) as xbar,
+                              t1.us_state
+                  from
+                    (select count(incident_id) as y, us_state from
+                              gun_incidents where incident_year = ${data.year} 
+                              group by us_state) t1,
+                              
+                            (select ${data.attribute} as x, us_state from
+                              ${attribute_to_table[data.attribute]} where ${
+      attribute_to_year_col[data.attribute]
+    } = ${data.year}
+                              ) t2
+                              
+                              where t1.us_state = t2.us_state
+                              group by t1.us_state
+                ) avgt,
+                      (
+                  select
+                    x, y
+                  from
+                    (select count(incident_id) as y, us_state from
+                              gun_incidents where incident_year = ${data.year} 
+                              group by us_state) t1,
+                              
+                              (select ${data.attribute} as x, us_state from
+                              ${attribute_to_table[data.attribute]} where ${
+      attribute_to_year_col[data.attribute]
+    } = ${data.year}
+                              ) t2
+                              
+                          where t1.us_state = t2.us_state
+                ) datat
+                    
+            ) r)`;
+  }
   oracledb.getConnection(
     {
       user: "jthies",
@@ -133,7 +214,7 @@ router.post("/", function(request, res) {
               }
               console.log(x_arr, y_arr, state_arr);
               res.render("regression_temp.handlebars", {
-                attribute: `"${data.attribute}"`,
+                attribute: `"${attribute_to_name[data.attribute]}"`,
                 state_arr: state_arr,
                 x_arr: x_arr,
                 y_arr: y_arr,
